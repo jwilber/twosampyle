f8e095464847f73e3baa9af000cab5f4b410224c
Jared Wilber <j.wilber92@gmail.com>
2017-04-08 14:13:03 -0700

    added scripts

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='twosampyle',
+      version='0.1',
+      description='Python module for two-sample statistical tests',
+      url='https://github.com/jwilber/twosampyle',
+      author='Jared Wilber',
+      author_email='jdwlbr@gmail.com',
+      license='GNU3',
+      packages=['twosampyle'],
+      zip_safe=False)
