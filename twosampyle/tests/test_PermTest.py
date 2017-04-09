d4c19f355d4c94f5ab49920ddf5398b165307fca
Jared Wilber <j.wilber92@gmail.com>
2017-04-09 03:33:35 -0700

    updated test_PermTestShouldLoadDataAsIs

--- twosampyle/tests/test_PermTest.py
+++ twosampyle/tests/test_PermTest.py
@@ -16,9 +16,9 @@ def test_InputNotCorrectShapesShouldRaiseError():
 def test_simPermDsnShouldReturnListOfSizeK():
 	pass
 
-def test_PermTestShouldLoadDataFine():
+def test_PermTestShouldLoadDataAsIs():
     from twosampyle.permtest import PermTest  # should this import always be in the test?
     data = {"trt1": [1,2,3,4,5,6,7,8,9,10], "trt2": [40,50,60,70,70,60,50,30,40,20]}
     df = pd.DataFrame(data)
     p = PermTest(df)
-    assert p.data == df
+    assert p.data.shape == df.shape
