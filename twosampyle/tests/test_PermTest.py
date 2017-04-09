bbba3cb4a3d958ad95de5f68f568ca42e5075050
Jared Wilber <j.wilber92@gmail.com>
2017-04-09 00:37:25 -0700

    updated test fail from indentation error

--- twosampyle/tests/test_PermTest.py
+++ twosampyle/tests/test_PermTest.py
@@ -17,7 +17,6 @@ def test_simPermDsnShouldReturnListOfSizeK():
 
 def test_PermTestShouldLoadDataFine():
     from PermTest import PermTest  # should this import always be in the test?
-    data = {"trt1": [1,2,3,4,5,6,7,8,9,10], 
-            "trt2": [40,50,60,70,70,60,50,30,40,20]}
+    data = {"trt1": [1,2,3,4,5,6,7,8,9,10], "trt2": [40,50,60,70,70,60,50,30,40,20]}
     df = pd.DataFrame(data)
-	p = PermTest(df)
+    p = PermTest(df)
