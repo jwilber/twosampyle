e84740c61c83e38149b3e0756dc0dcdf7f0aa6c8
Jared Wilber <j.wilber92@gmail.com>
2017-04-09 00:32:05 -0700

    added new test

--- twosampyle/tests/test_PermTest.py
+++ twosampyle/tests/test_PermTest.py
@@ -1,4 +1,23 @@
 import pytest
+import pandas as pd 
+import numpy as np 
+
 
 def test_works():
 	pass
+
+def test_DiffMeansForSameDataShouldBeZero():
+	pass
+
+def test_InputNotCorrectShapesShouldRaiseError():
+	pass
+
+def test_simPermDsnShouldReturnListOfSizeK():
+	pass
+
+def test_PermTestShouldLoadDataFine():
+    from PermTest import PermTest  # should this import always be in the test?
+    data = {"trt1": [1,2,3,4,5,6,7,8,9,10], 
+            "trt2": [40,50,60,70,70,60,50,30,40,20]}
+    df = pd.DataFrame(data)
+	p = PermTest(df)
