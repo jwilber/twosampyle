aec2cd0ed4caf3fe20497ec9cd8ad46519a73fe8
Jared Wilber <j.wilber92@gmail.com>
2017-04-09 01:37:12 -0700

    added permtest name

--- twosampyle/tests/test_PermTest.py
+++ twosampyle/tests/test_PermTest.py
@@ -1,6 +1,8 @@
 import pytest
+from twosampyle import permtest
 import pandas as pd 
 import numpy as np 
+import matplotlib.pyplot as plt 
 
 
 def test_works():
@@ -16,7 +18,7 @@ def test_simPermDsnShouldReturnListOfSizeK():
 	pass
 
 def test_PermTestShouldLoadDataFine():
-    from twosampyle import PermTest  # should this import always be in the test?
+    from permtest import PermTest  # should this import always be in the test?
     data = {"trt1": [1,2,3,4,5,6,7,8,9,10], "trt2": [40,50,60,70,70,60,50,30,40,20]}
     df = pd.DataFrame(data)
     p = PermTest(df)
