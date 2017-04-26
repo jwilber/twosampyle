8ba18589ba5da130898be89cf5d4ba902498b66e
Jared Wilber <j.wilber92@gmail.com>
2017-04-26 01:28:34 -0700

    test for two-sided pvalue (t-test)

--- twosampyle/tests/test_TTest.py
+++ twosampyle/tests/test_TTest.py
@@ -10,11 +10,11 @@ def test_pValueShouldBeBetweenZeroAndOne():
 	assert 0 <= tt.pvalue() <= 1
 
 def test_pValueShouldBeVeryCloseToZeroIfDataExtremelyDifferent(): 
-	tt = TTest([1,2,3,4,5,6,7,4,4,4,3,2,1,2,1.5,8,1,2,3],
+	tt = TTest([1]*19],
 		[100000,1000,1000,1000,4000,5000,6000,24000,-4000,5000,60000,40000,555555,33333355555555,434343,43434,1e5,2e5,4e5])
-	assert tt.pvalue() < 1e-4
+	assert tt.pvalue(sided='two') < 1e-4
 
 
 def test_pValueShouldBeOneIfDataExactlyTheSame(): 
-	tt = TTest()
+	tt = TTest(sided='two')
 	assert tt.pvalue(range(100000),range(100000)) >= .9999
--- twosampyle/ttest.py
+++ twosampyle/ttest.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from scipy.stats import t
 
-class TTest():
+cclass TTest():
     
     def __init__(self, a=None, b=None):
         self.a = a
@@ -10,7 +10,7 @@ class TTest():
         
     @staticmethod
     def t_statistic(a,b):
-        """"""
+        """Calculates t-statistic and degrees of freedom for input data"""
         N_a = len(a)
         N_b = len(b)
         mu_a = np.mean(a)
@@ -26,42 +26,44 @@ class TTest():
             # t_denom = np.sqrt(2. / N_a) * np.sqrt( (var_a + var_b) / 2 )
             t_denom = np.sqrt( 1.*((N_a-1)*var_a + (N_b-1)*var_b) / (N_a + N_b - 2) ) * np.sqrt( 1./N_a + 1./N_b )
             t_stat = 1. * t_num / t_denom
-            return t_stat
+            # degrees of freedom for equal variance
+            df = N_a + N_b - 2
+            return t_stat, df
         
         # unequal variance
         else:
             t_num = mu_a - mu_b
             t_denom = np.sqrt( 1. * var_a / N_a + 1. * var_b / N_b)
             t_stat = 1. * t_num / t_denom
-            return t_stat
+            # degrees of freedom for unequal variance
+            num = (1.*var_a/N_a + 1.*var_b/N_b)**2
+            denom = (1./(N_a-1))*(1.*var_a/N_a)**2 + (1./(N_b-1))*(1.*var_b/N_b)**2
+            df = 1. * num / denom
+            return t_stat, df
 
         
     def testStat(self, input_a=None, input_b=None):
-        """"""
+        """Calculates test statistic using the static class method, `t_statistic`"""
         if input_a or input_b:
-            ttest_stat = TTest.t_statistic(input_a, input_b)
+            ttest_stat, df = TTest.t_statistic(input_a, input_b)
         else:
-            ttest_stat = TTest.t_statistic(self.a, self.b)
-        return ttest_stat
+            ttest_stat, df = TTest.t_statistic(self.a, self.b)
+        return ttest_stat, df
     
     
-    def pvalue(self, input_a=None, input_b=None, sided='one'):
-        """"""
+    def pvalue(self, input_a=None, input_b=None, sided='two'):
+        """Calculates the p-value for our test. Default is two-sided"""
         if input_a:
             N = len(input_a)
         else:
             N = len(self.a)
             
-        # degrees of freedom
-        df = 2*N - 2
-        # test-statistic
-        test_statistic = self.testStat(input_a, input_b)
+        
+        # test-statistic & degrees of freedom
+        test_statistic, df = self.testStat(input_a, input_b)
         
         # p-value 
         p = 1 - t.cdf(np.abs(test_statistic), df=df) 
-        
-#         if sided != 'one' or sided != 'two':
-#             raise ValueError("sided must be either 'one' or 'two'")
         if sided == 'one':
             p_value = p
         else: # two-sided
@@ -71,14 +73,13 @@ class TTest():
     
     
     def plot_dsn(self, input_a = None, input_b = None):
-        """"""
+        """Plots our p-value in a t-distribution"""
         if input_a:
             N = len(input_a)
         else:
             N = len(self.a)
-        df = 2*N - 2
         
-        test_stat = self.testStat(input_a, input_b)
+        test_stat, df = self.testStat(input_a, input_b)
         fig, ax = plt.subplots(1, 1)
         x = np.linspace(t.ppf(0.01, df), 
                         t.ppf(0.99, df), 100)
@@ -89,4 +90,3 @@ class TTest():
         ax.set_title('T-Distribution with {}. D.o.F.'.format(df))
         plt.axvline(test_stat, lw=7)
         plt.show()
-
